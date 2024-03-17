5ebc01d5b4e3571012dde7959d68bf1796f74952
MuhFauzanMajid <muhammadfauzanmajid@gmail.com>
2024-03-17 19:10:10 +0700

    merge

--- tpmodul5_1302220144/Program.cs
+++ tpmodul5_1302220144/Program.cs
@@ -4,6 +4,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        HaloGeneric halo = new HaloGeneric();
+        //testing input string
+        halo.SapaUser("Majid");
+        //testing input integer
+        halo.SapaUser(123);
+        //testing input float
+        halo.SapaUser(123.321);
+
+        Console.WriteLine();
+        //testing input integer
+        DataGeneric<int> dataInt = new DataGeneric<int>(1302220144);
+        dataInt.PrintData();
     }
 }
