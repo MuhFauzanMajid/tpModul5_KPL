eae112686b28a491fd03749ff06915431cf22364
MuhFauzanMajid <muhammadfauzanmajid@gmail.com>
2024-03-17 18:51:38 +0700

    menambahkan class DataGeneric

---
+++ tpmodul5_1302220144/DataGeneric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302220144
+{
+    public class DataGeneric<T>
+    {
+        public T Data { get; set; }
+
+        public DataGeneric(T data)
+        {
+            Data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+        }
+    }
+}
--- tpmodul5_1302220144/Program.cs
+++ tpmodul5_1302220144/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using tpmodul5_1302220144;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //testing input integer
+        DataGeneric<int> dataInt = new DataGeneric<int>(1302220144);
+        dataInt.PrintData();
+
+        //testing input string
+        DataGeneric<string> dataString = new DataGeneric<string>("1302220144");
+        dataString.PrintData();
     }
 }
